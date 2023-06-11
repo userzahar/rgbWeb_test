aa08848559dbbb531e15ac9602c3473e574c4580
userzahar <110730909+userzahar@users.noreply.github.com>
2023-06-11 22:13:57 +0300

    fix Container

--- src/components/Container/Container.Styled.js
+++ src/components/Container/Container.Styled.js
@@ -1,11 +1,12 @@
 import { styled } from "styled-components";
 
 export const ContainerStyled = styled.div`
-  width: 100%;
+  width: 427px;
   height: 474px;
 
   @media screen and (max-width: 768px) {
     display: flex;
+    width: 100%;
     flex-direction: column;
     align-items: center;
     height: 228px;
